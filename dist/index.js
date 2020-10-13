eecfbd9c60f31ab58d237c4b876743a2ddca69f4
lsj_0517 <lsj_0517@sbs.co.kr>
2020-10-13 18:01:45 +0900

    init example page

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,44 @@
-module.export = {};
+class Subtyle {
+  constructor(video, track) {
+    this.videoElement = video;
+    this.videoVtt = video.textTracks;
+    this.track = track;
+
+    this.style = {
+      animate: true,
+      background: 'black',
+      color: 'white',
+    };
+
+    this.vtt = (option) => {
+      this.render();
+    };
+
+    this.big = (option) => {};
+
+    this.small = (option) => {};
+
+    this.white = (option) => {};
+
+    this.show = () => {};
+
+    this.hide = () => {};
+  }
+
+  render() {
+    let subtitleContainer = document.createElement('div');
+    let subtitleText = document.createElement('span');
+    subtitleContainer.appendChild(subtitleText);
+
+    this.videoElement.parentNode.prepend(subtitleContainer);
+
+    this.track.addEventListener('cuechange', (event) => {
+      console.log(event);
+      if (event.target.track.activeCues[0] !== undefined) {
+        subtitleText.textContent = event.target.track.activeCues[0].text;
+      } else {
+        subtitleText.textContent = '';
+      }
+    });
+  }
+}
